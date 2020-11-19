adce8b3195c9f9b6194af8ea3b57b15a209506b1
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-19 21:19:27 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,6 +30,8 @@ class PetsController < ApplicationController
   get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     @owners = Owner.all
+
+    erb :'pets/edit.erb'
   end
 
   patch '/pets/:id' do
