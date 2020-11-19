c947e935a2b4dca619bcc72913b90e0018f54327
Dominic Barajas <dominicbarajas548@gmail.com>
2020-11-19 21:10:59 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,14 +11,16 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    @pet = Pet.create(params[:pet])
+   @pet = Pet.create(params[:pet])
 
-    if !params["owner"]["name"].empty?
-      @pet.owner << Owner.create(name: params["owner"]["name"])
-    end
-    @pet.save
-    redirect to "pets/#{@pet.id}"
-  end
+   if !params["owner"]["name"].empty?
+     @pet.owner = Owner.create(name: params["owner"]["name"])
+   end
+
+   @pet.save
+
+   redirect to "pets/#{@pet.id}"
+ end
 
   get '/pets/:id' do
     @pet = Pet.find(params[:id])
